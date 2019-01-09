d551dab36a34b9c065e9e90af5dc85c108187d21
Pranav Burugula <pranav.burugula@gmail.com>
2019-01-09 12:39:39 -0500

    removed old autonomous selector

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,8 +10,6 @@ package frc.robot;
 import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.driveSystems.DriveSystem;
 import frc.robot.jsonReaders.AttachmentsReader;
 import frc.robot.jsonReaders.DriveSysReader;
@@ -29,8 +27,6 @@ import frc.robot.util.LogitechF310;
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
   private RobotConfigReader robotConfigReader;
   private AttachmentsReader attachmentsReader;
   private LogitechF310 gamepad1;
@@ -45,10 +41,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_chooser.addDefault("Default Auto", kDefaultAuto);
-    m_chooser.addObject("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-
     robotConfigReader = new RobotConfigReader("robotName");
 
     driveSysReader = new DriveSysReader(robotConfigReader.getDriveSysName());
@@ -90,9 +82,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+    
   }
 
   /**
@@ -100,15 +90,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
+
   }
 
   /**
